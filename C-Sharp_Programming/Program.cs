f4e63df8c51870bda9a9a7624567a36353309db5
Akash <parmar.aakash26@gmail.com>
2025-07-01 16:28:27 +0530

    WriteLine and ReadLine print

--- C-Sharp_Programming/Program.cs
+++ C-Sharp_Programming/Program.cs
@@ -11,11 +11,29 @@ namespace C_Sharp_Programming
     {
         static void Main(string[] args)
         {
-            string input = "Hello, World!";
-            Console.WriteLine("Hi i'am Akash" + input);
-            Console.WriteLine("From: Gujarat");
-            Console.WriteLine("Developer");
+            // string input = "Hello, World!";
+            // Console.WriteLine("Hi i'am Akash" + input);
+            // Console.WriteLine("From: Gujarat");
+            // Console.WriteLine("Developer");
 
+            // //Read Line Example
+
+            //string userName = Console.ReadLine();
+
+            // Console.WriteLine("Hello " + userName);
+
+            // //Always use place holder
+
+            // Console.WriteLine("Hello {0}", userName);
+
+            // both print lastName, FirstName
+            Console.WriteLine("Please Enter your First Name");
+            string firstName =Console.ReadLine();
+
+            Console.WriteLine("Please Enter your Last Name");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0}, {1}", lastName, firstName);
         }
     }
 }
