ccf6d1999222a0ad47ca09b0ef4deb0a15a046c7
Akash <parmar.aakash26@gmail.com>
2025-07-14 15:36:38 +0530

    objects update

--- 09_Class/Program.cs
+++ 09_Class/Program.cs
@@ -5,7 +5,21 @@
 // We seen simple data types like : Int, Float, String, etc. If ypu want to create complex custom types, then we can make use of classes.
 
 // A class consists of data and behavior. Class data is represented by fields, and behavior is represented by methods.
-// Constructor : 
+
+
+/* 
+ * C# programs are composed of classies that represent the entities of program which also include code to instantiate the classes as objects
+ * 
+ * when the programs runs, objects are created for the classes and they may interact with each other to provide the functionalities of the program
+ * 
+ * An object is a tangible entity such as a car, a table  or person.every object has some characteristics and is capable of performing certain actions.
+ * 
+ * An object in a programming langauage has a unique identity, state and behavior.
+ * 
+ * The state of the object refers to its attributes or properties, which are represented by fields in a class.
+ * 
+ * An object's has various features that can describe it which could be the company name, nodel, color etc.
+ */
 
 using System;
 
